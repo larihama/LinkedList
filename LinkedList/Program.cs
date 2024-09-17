f6e134bf8931ab9ce6f3aa9dbd4b46f931af7df4
larihama <lari.03@icloud.com>
2024-09-17 22:33:52 +0300

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+        }
+
+        // Kopioidaan taulukon arvot listaan
+        List<int> numberList = new List<int>(numbers);
+
+        numberList.Sort();
+
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        foreach (int number in numberList)
+        {
+            linkedList.AddLast(number);
+        }
+
+        Console.WriteLine("10 random integers:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nList in ascending order:");
+        foreach (int number in numberList)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nLinkedList:");
+        foreach (int number in linkedList)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
